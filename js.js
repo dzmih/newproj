010af1b28d9c93ff21928611fa6636ab2a64cdba
dzmih <daniil.maksim1995@gmail.com>
2023-09-06 13:24:58 +0300

    practice js

---
+++ js.js
@@ -0,0 +1,40 @@
+// const bodyElement = document.body;
+// const firstChildNode = bodyElement.first Child;
+// const lastChildNode = bodyElement.lastChild;
+// const childNodes = bodyElement.childNodes;
+// console.log(firstChildNode)
+// console.log(lastChildNode)
+// console.log(childNodes)
+// for(let node of childNodes){
+//     console.log(node)
+// }
+// const a = bodyElement.getElementsByClassName("h1")
+// console.log(a)
+// const a1 = a.outerHTML;
+// console.log(a1)
+// let i = 0
+// let a = 123124
+// for(let i = 0; a != 0; i++){
+//     a--;
+//     console.log(a)
+// }
+// let doc = document.body;
+// console.log(doc.querySelectorAll('ul > li'))
+// let number = 0
+// console.log(number)
+// obj = {
+//     1: 3,
+//     2: 2,
+//     3: 1,
+// }
+// console.log(obj)
+// obj = {
+
+// }
+// obj['name'] = prompt("your name: ", "")
+// obj['surname'] = prompt("your surname: ", "")
+// obj['age'] = +prompt("your age: ")
+// console.log(obj)
+let a = 0
+let b = 0
+console.log(a, b)
